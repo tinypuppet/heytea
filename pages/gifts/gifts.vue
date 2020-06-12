3cf7858cab06ad877819615e186e53930d6bdab4
9bc_100007986463_tinypuppet <9bc_100007986463_tinypuppet@git.cloud.tencent.com>
2020-06-12 15:59:21 +0800

    阿喜有礼

---
+++ pages/gifts/gifts.vue
@@ -0,0 +1,19 @@
+<template>
+	<view>
+		<image src="https://static.heytea.com/taro_trial/v1/img/my/img_gift_card_banner.png?timestamp=1591947928848"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
