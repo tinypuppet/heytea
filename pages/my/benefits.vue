5335ffa1a495d1290d440976487637fb55e8ac42
tinypuppet <tinypuppet@163.com>
2020-06-17 22:57:14 +0800

    我的-会员权益

---
+++ pages/my/benefits.vue
@@ -0,0 +1,20 @@
+<template>
+	<view class="container">
+		<image src="https://static.heytea.com/taro_trial/v1/img/my/member_benefits/me_rights_img_go.png"></image>
+		<image src="https://static.heytea.com/taro_trial/v1/img/my/member_benefits/level_1-6_bg.png"></image>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
